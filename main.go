e848d9a3d67d47f708a987e9ee97f34aacd4a86b
petronem <matt.petrone@carii.com>
2021-04-06 22:07:47 -0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Arbitrary sleep so that we can demonstrate autoscaler
+	time.Sleep(101 * time.Millisecond)
+	fmt.Fprintln(w, "Hi there, I'm running in Rio v1")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
